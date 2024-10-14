f2c524eb4027c0757300b4c318c377efbc88a33b
Gaguine <gbutera12@gmail.com>
2024-10-14 13:39:56 +0300

    ex 6, if how not given. v2

--- Homework/Homework1.py
+++ Homework/Homework1.py
@@ -123,14 +123,17 @@ https://pavel-karateev.gitbook.io/intermediate-python/sintaksis/args_and_kwargs
  Если правила не переданы, следует напечатать списки с sep=' ' и end='\n'.
 """
 def print_lists(*args :list, how=None ):
-    sep = ' '
-    end = '\n'
+    if how == None:
+        how = {
+            'sep' : ' ',
+            'end' : '\n'
+        }
     for arg in args:
         for index, element in enumerate(arg):
             if index < len(arg) - 1:
-                print(element, end=sep)
+                print(element, end=how['sep'])
             else:
-                print(element, end=end)
+                print(element, end=how['end'])
 print_lists([1,2,3], [4,5,6], [7,8,9])
 
 
