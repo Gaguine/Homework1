f16937d595b498bc1492d0e2613631981ad3e831
Gaguine <gbutera12@gmail.com>
2024-10-26 13:01:41 +0300

    Task 1 and flowers added.

---
+++ Homework 2/Task 1.py
@@ -0,0 +1,19 @@
+# HW2. Task 1.1
+
+'''Имплементируйте функцию, которая опишет датасет в папке class_3/hw2/t2_data/flowers,
+где объекты это изображения, названия папок отвечают атрибуту "type" и в названии каждого изображения содержится информация о цвете цветка *_<color>.jpeg
+
+Json structure example:
+
+{
+    "flowers":
+    [
+        {
+            "path": "flowers/daisy/african_daisy_yellow.jpeg",
+            "color": "yellow",
+            "type": "daisy"
+        },
+        ...
+    ]
+}
+'''
