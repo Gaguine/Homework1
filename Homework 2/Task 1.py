2eb5d1366068187148450e09e64e72f24ed8e40e
Gaguine <gbutera12@gmail.com>
2024-10-26 17:14:43 +0300

    Still working on task 1.2. dataset_fetcher improved.

--- Homework 2/Task 1.py
+++ Homework 2/Task 1.py
@@ -38,7 +38,9 @@ def dataset_fetcher(path:str) -> dict:
     for flower_type, files in zip(type_list, color_list):
         for file_name in files:
             # Extract color from the file name using split
-            color = file_name
+            color = file_name.replace('.jpeg','') # remove .jpeg
+            if flower_type in color:  # unfortunately this line of code does not work((
+                color.replace(flower_type,'')
             #Create blueprint
             flower_dic = {'path': f'{path}/{flower_type}/{color}',
                           'color': color,
@@ -50,14 +52,23 @@ def dataset_fetcher(path:str) -> dict:
 
 
 path = 'G:/PyCharm Projects/Volgastate_Coding_Course/Homework 2/Files/flowers'
-flowers_dic = dataset_fetcher(path)
-json_converter(flowers_dic,path)
+# flowers_dic = dataset_fetcher(path)
+# json_converter(flowers_dic,path)
 # HW2. Task 1.2
 class FlowersDataset:
     def __init__(self,json_file_path: str): # provide the object with a dataset property
       with open(json_file_path, "r", encoding='utf-8') as file:
           self.data = json.load(file) # read through the json file and store the data as a dictonary.
-    # def get_items(self): not sure how to implement???
+    def get_items(self, properties: dict) -> [list[str], str]:
+        output_list=[]
+        if properties in self.data["flowers"]:
+            print("hello!")
 
 
+json_file_path = f'{path}/flower_data.json'
+flowers_database = FlowersDataset(json_file_path)
+properties = {"type": "rose"}
+flowers_database.get_items(properties)
+# flower_database.get_items({'type':'rose', 'color': 'red'}) --> flowers: list of dict # Domanda! flowers[0] e uguale al primo elemento??
+#quando ti chiedo coppia, controlla per tutta la lista e ritorna lista dei nomi dei file!!
 
