280a6f48a5c4e5fcda7f3b8f3db5fd5db8a7e25f
Gaguine <gbutera12@gmail.com>
2024-10-26 15:19:35 +0300

    created dataset_fetcher. json_converter refined. Task 1.1 completed.

--- Homework 2/Task 1.py
+++ Homework 2/Task 1.py
@@ -1,13 +1,5 @@
 import os
 import json
-
-def json_converter(flowers_dic: dict, path : str, file_name = "flower_data.json") -> json:
-    """Mark where the json file will be created."""
-    json_file_path = f'{path}/{file_name}'
-    with open(json_file_path, 'w', encoding="UTF-8") as file:
-        json.dump(flowers_dic,  file, sort_keys=False, indent=2, ensure_ascii=False)
-
-
 # HW2. Task 1.1
 '''Имплементируйте функцию, которая опишет датасет в папке class_3/hw2/t2_data/flowers(путь будет отличатся:G:\PyCharm Projects\Volgastate_Coding_Course\Homework 2\Files\flowers),
 где объекты это изображения, названия папок отвечают атрибуту "type" и в названии каждого изображения содержится информация о цвете цветка *_<color>.jpeg
@@ -26,27 +18,39 @@ Json structure example:
     ]
 }
 '''
-flowers_dic = {'flowers': []} # initialize essential variables
+def json_converter(flowers_dic: dict, path : str, file_name = "flower_data.json") -> json:
+    """Mark where the json file will be created."""
+    json_file_path = f'{path}/{file_name}'
+    with open(json_file_path, 'w', encoding="UTF-8") as file:
+        json.dump(flowers_dic,  file, sort_keys=False, indent=2, ensure_ascii=False)
+def dataset_fetcher(path:str) -> dict:
+    """creates a dictionary using the provided path"""
+    flowers_dic = {'flowers': []}  # initialize output variable
+
+    type_list = []  # initialize lists to hold types and colors
+    color_list = []
+    x = os.listdir(path)
+
+    for flower_type in x:  # collect flower types and colors
+        type_list.append(flower_type)
+        color_list.append(os.listdir(f'{path}/{flower_type}'))
+
+    for flower_type, files in zip(type_list, color_list):
+        for file_name in files:
+            # Extract color from the file name using split
+            color = file_name
+            #Create blueprint
+            flower_dic = {'path': f'{path}/{flower_type}/{color}',
+                          'color': color,
+                          'type': flower_type}
+            # Store blueprints
+            flowers_dic['flowers'].append(flower_dic)
+
+    return flowers_dic
+
+
 path = 'G:/PyCharm Projects/Volgastate_Coding_Course/Homework 2/Files/flowers'
-
-type_list = [] # initialize lists to hold types and colors
-color_list = []
-
-x = os.listdir(path)
-for flower_type in x: # collect flower types and colors
-    type_list.append(flower_type)
-    color_list.append(os.listdir(f'{path}/{flower_type}'))
-
-for flower_type, files in zip(type_list, color_list):
-    for file_name in files:
-        # Extract color from the file name using split
-        color = file_name
-
-
-        flower_dic = {'path':f'{path}/{flower_type}/{color}',
-                      'color': color,
-                      'type': flower_type}
-
-        flowers_dic['flowers'].append(flower_dic)
-
+flowers_dic = dataset_fetcher(path)
 json_converter(flowers_dic,path)
+
+
