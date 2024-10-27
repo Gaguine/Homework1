9685ad647cb249607aeb743efa993bd945dd6397
Gaguine <gbutera12@gmail.com>
2024-10-27 11:49:13 +0300

    trying to implement all() in Task 1.2

--- Homework 2/Task 1.py
+++ Homework 2/Task 1.py
@@ -1,23 +1,6 @@
 import os
 import json
 # HW2. Task 1.1
-'''Имплементируйте функцию, которая опишет датасет в папке class_3/hw2/t2_data/flowers(путь будет отличатся:G:\PyCharm Projects\Volgastate_Coding_Course\Homework 2\Files\flowers),
-где объекты это изображения, названия папок отвечают атрибуту "type" и в названии каждого изображения содержится информация о цвете цветка *_<color>.jpeg
-
-Json structure example:
-
-{
-    "flowers":
-    [
-        {
-            "path": "flowers/daisy/african_daisy_yellow.jpeg",
-            "color": "yellow",
-            "type": "daisy"
-        },
-        ...
-    ]
-}
-'''
 def json_converter(flowers_dic: dict, path : str, file_name = "flower_data.json") -> json:
     """Mark where the json file will be created."""
     json_file_path = f'{path}/{file_name}'
@@ -49,27 +32,27 @@ def dataset_fetcher(path:str) -> dict:
 
     return flowers_dic
 
-
+"""Test to see if the functions work"""
 path = 'G:/PyCharm Projects/Volgastate_Coding_Course/Homework 2/Files/flowers'
-flowers_dic = dataset_fetcher(path)
-json_converter(flowers_dic,path)
+# flowers_dic = dataset_fetcher(path)
+# json_converter(flowers_dic,path)
 # HW2. Task 1.2
 class FlowersDataset:
     def __init__(self,json_file_path: str): # provide the object with a dataset property
       with open(json_file_path, "r", encoding='utf-8') as file:
-          self.data = json.load(file) # read through the json file and store the data as a dictonary.
+          self.data = json.load(file) # read through the json file and store the data as a dictionary.
     def get_items(self, properties: dict) -> [list[str], str]:
-        output_list=[]
+        output_list = []
         for flower in self.data['flowers']:
-            for key,value in properties.items():
-                if properties[key] == flower[key] or properties[key] in flower[key]:
+            for key, value in properties.items():
+                if all(flower.get(key) == value):
                     output_list.append(flower['path'])
         return output_list
 
 
-# json_file_path = f'{path}/flower_data.json'
-# flowers_database = FlowersDataset(json_file_path)
-# properties = {"color": "red"}
-# print(flowers_database.get_items(properties))
+json_file_path = f'{path}/flower_data.json'
+flowers_database = FlowersDataset(json_file_path)
+properties = {"type":"rose","color": "red"}
+print(flowers_database.get_items(properties))
 
 
