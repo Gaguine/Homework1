59ea394874b1608d6a00c8fb448bb7158368d5af
Gaguine <gbutera12@gmail.com>
2024-10-27 12:10:28 +0300

    Tasks 1.1 and 1.2 revised. Color storage to fix.

--- Homework 2/Task 1.py
+++ Homework 2/Task 1.py
@@ -2,16 +2,16 @@ import os
 import json
 # HW2. Task 1.1
 def json_converter(flowers_dic: dict, path : str, file_name = "flower_data.json") -> json:
-    """Mark where the json file will be created."""
+    """Creates a Json file on a designated path using the data from a dictionary"""
     json_file_path = f'{path}/{file_name}'
     with open(json_file_path, 'w', encoding="UTF-8") as file:
         json.dump(flowers_dic,  file, sort_keys=False, indent=2, ensure_ascii=False)
 def dataset_fetcher(path:str) -> dict:
-    """creates a dictionary using the provided path"""
+    """Creates a dictionary using the provided path"""
     flowers_dic = {'flowers': []}  # initialize output variable
     rel_path = path.replace('G:/PyCharm Projects/Volgastate_Coding_Course/','')
 
-    type_list = []  # initialize lists to hold types and colors
+    type_list = []  # Initialize lists to hold types and colors
     color_list = []
     x = os.listdir(path)
 
@@ -31,11 +31,6 @@ def dataset_fetcher(path:str) -> dict:
             flowers_dic['flowers'].append(flower_dic)
 
     return flowers_dic
-
-"""Test to see if the functions work"""
-path = 'G:/PyCharm Projects/Volgastate_Coding_Course/Homework 2/Files/flowers'
-# flowers_dic = dataset_fetcher(path)
-# json_converter(flowers_dic,path)
 # HW2. Task 1.2
 class FlowersDataset:
     def __init__(self,json_file_path: str): # provide the object with a dataset property
@@ -48,10 +43,21 @@ class FlowersDataset:
                 output_list.append(flower['path'])
         return output_list
 
+"""Test to see if the functions work"""
+path = 'G:/PyCharm Projects/Volgastate_Coding_Course/Homework 2/Files/flowers'
+# flowers_dic = dataset_fetcher(path)
+# json_converter(flowers_dic,path)
 
+"""Test to see if class and method work properly"""
 json_file_path = f'{path}/flower_data.json'
 flowers_database = FlowersDataset(json_file_path)
 properties = {"type": "tulip", "color": "magenta"}
 print(flowers_database.get_items(properties))
 
-# ({"type": ["rose", "tulip"], "color": "red"})
+"""
+Properties tested:
+- {"type": ["rose", "tulip"], "color": "red"}
+- {"type": "rose", "color": "red"}
+- {"type: "tulip", "color": ["red", "blue"]}
+- {"type: "tulip", "color": "magenta"}
+"""
