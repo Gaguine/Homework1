5a835e4755f2bb822c06fd612bcd509dfed00c33
Gaetano <gbutera12@gmail.com>
2024-11-09 12:04:52 +0300

    Created term frequency function. The function does not pass the 2 output test. To fix. To add the os module and fix the relative path.

--- Homework 3/Task1.py
+++ Homework 3/Task1.py
@@ -1,6 +1,6 @@
 import json
 import os
-from numpy.ma.core import count
+# from numpy.ma.core import count
 from typing import List
 
 def read_data(path) -> List[str] :
@@ -30,15 +30,29 @@ def chapters_chapters_dict(data:list)->List[List[str]]:
             chapter=[]
     return chapters_list
 def chapter_frequency(target_word: str, data: List[str]) -> float:
-    """This functin returns the occurance frequency of the target word within the txt file, represented by the chapters list."""
+    """This function returns the occurrence frequency of the target word within the txt file, represented by the chapters list."""
     number_of_chapter_with_target_word = 0
     chapters_list = chapters_chapters_dict(data)
     for chapter in chapters_list:
         if target_word in chapter: number_of_chapter_with_target_word += 1
 
-    return number_of_chapter_with_target_word / len(chapters_list)
+    return round(number_of_chapter_with_target_word/len(chapters_list),2) # round the number so it matches the example output
+def term_frequency(target_word: str, chapter_number: int) -> float:
+    data = read_data(path) # this is a list
+    tot_num_words_chapter = 0
+    target_word_counter = 0 # these will be used for the output formula
 
-path = 'G:\PyCharm Projects\Homework1\Homework 3\war_peace.txt'
+    chapters_list = chapters_chapters_dict(data)
+    for word in chapters_list[chapter_number]: #check if the target word is the selected chapter
+        tot_num_words_chapter +=1
+        if word==target_word:
+            target_word_counter += 1
+    return round(target_word_counter/tot_num_words_chapter,4)
+
+
+# path = 'G:\PyCharm Projects\Homework1\Homework 3\war_peace.txt' # absolute path for windows
+path = '/Users/giuseppe/PycharmProjects/Homework1/Homework 3/war_peace.txt' # absolute path for macos
 data = read_data(path)
-target_word = 'война'
-print(chapter_frequency(target_word,data))
+target_word = 'мир'
+chapter_number=137
+print(term_frequency(target_word,chapter_number)) # la funzione non passa il test con la parola 'pace'
