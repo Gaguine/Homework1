66569c89518923873bcd1e84d1fb70747543fc16
Gaguine <gbutera12@gmail.com>
2024-11-17 12:14:13 +0300

    Task 1. Step 3 completed.

--- Homework 4/Task 1.py
+++ Homework 4/Task 1.py
@@ -14,26 +14,29 @@ def file_reader(file_name: str) -> str:
             text += phrase
     return text
 def frequency_dictionary_creator(contents: str) -> Dict:
-    """The following function creates a dictionary from a string. The dictionary contains the word, the number of times it appeared in the text."""
+    """The following function creates a dictionary from a string. The function sorts the contents of the str,
+    transforming the words into their "normal form", and check whether the word is a preposition.The dictionary contains
+     the word, the number of times, it appeared in the text and its respective TF. Prepositions are not stored in the dictionary."""
+
+    morph = pymorphy3.MorphAnalyzer()
     frequency_dictionary = {}
     total_num_words = len(contents.split(" "))
+
     for word in contents.split(' '):
-        if word not in frequency_dictionary:
-            frequency_dictionary[word] = [0, 0.0]
-        frequency_dictionary[word][0] +=1
-        if frequency_dictionary[word][0] != 0:
-            frequency_dictionary[word][1] = round(frequency_dictionary[word][0]/total_num_words, 3)
+        if str(morph.parse(word)[0].tag) != "PREP": # Test if the iterated word is a preposition, if it is save it in the dictionary
+            if word not in frequency_dictionary:
+                    normal_form_word = morph.parse(word)[0].normal_form
+                    frequency_dictionary[normal_form_word] = [0, 0.0]
+            frequency_dictionary[normal_form_word][0] +=1
+            if frequency_dictionary[normal_form_word][0] != 0:
+                frequency_dictionary[normal_form_word][1] = round(frequency_dictionary[normal_form_word][0]/total_num_words, 3)
     return frequency_dictionary
 
 
 
-# Store the contents of txt file in variables and use lowercase method.
-positive = file_reader('positive').lower()
-negative = file_reader('negative').lower()
-
-#Remove punctuation
-positive = positive.translate(str.maketrans('','',string.punctuation))
-negative = negative.translate(str.maketrans('','',string.punctuation))
+# Store the contents of txt file in variables and use lowercase method and remove the punctuation.
+positive = file_reader('positive').lower().translate(str.maketrans('','',string.punctuation))
+negative = file_reader('negative').lower().translate(str.maketrans('','',string.punctuation))
 
 # Remove extra whitespace
 positive = ' '.join(positive.split())
