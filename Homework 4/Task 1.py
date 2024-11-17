5ee048eebbb3d82764eee06ec3d476e2e1da5239
Gaguine <gbutera12@gmail.com>
2024-11-17 11:42:05 +0300

    Task 1. created frequency dictionary function.

--- Homework 4/Task 1.py
+++ Homework 4/Task 1.py
@@ -1,5 +1,6 @@
 import os, string, re
 from os.path import pathsep
+from typing import Dict, Union, List
 from uu import decode
 import pymorphy3, matplotlib
 
@@ -12,16 +13,34 @@ def file_reader(file_name: str) -> str:
         for phrase in file:
             text += phrase
     return text
+def frequency_dictionary_creator(contents: str) -> Dict:
+    """The following function creates a dictionary from a string. The dictionary contains the word, the number of times it appeared in the text."""
+    frequency_dictionary = {}
+    total_num_words = len(contents.split(" "))
+    for word in contents.split(' '):
+        if word not in frequency_dictionary:
+            frequency_dictionary[word] = [0, 0.0]
+        frequency_dictionary[word][0] +=1
+        if frequency_dictionary[word][0] != 0:
+            frequency_dictionary[word][1] = round(frequency_dictionary[word][0]/total_num_words, 3)
+    return frequency_dictionary
+
+
 
 # Store the contents of txt file in variables and use lowercase method.
 positive = file_reader('positive').lower()
 negative = file_reader('negative').lower()
 
 #Remove punctuation
-positive, negative = positive,negative.translate(str.maketrans('','',string.punctuation))
+positive = positive.translate(str.maketrans('','',string.punctuation))
+negative = negative.translate(str.maketrans('','',string.punctuation))
 
 # Remove extra whitespace
 positive = ' '.join(positive.split())
 negative = ' '.join(negative.split())
 
-print(negative)
+# Create a frequency dictionary, contains the number of times a word occurred and its respective tf
+positive_dict = frequency_dictionary_creator(positive)
+negative_dict = frequency_dictionary_creator(negative)
+
+print(negative_dict)
